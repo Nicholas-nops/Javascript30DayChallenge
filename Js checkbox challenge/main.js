400f6b97c0651cdfa86dfe2fef0a4ec9fafea1f8
Nicholas-nops <nicholaspacidonio@gmail.com>
2020-05-28 17:37:56 -0300

    checkbox checklist

---
+++ Js checkbox challenge/main.js
@@ -0,0 +1,20 @@
+let lastChecked;
+function onSelect(e) {
+    
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox == this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', onSelect));
